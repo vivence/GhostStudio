1152927984f590003230d3b1557d7ec8e3fef9b0
Evil.Ghost <shawn.ghost.lee@gmail.com>
2016-02-25 16:09:28 +0800

    资源持有者
    流持有者

---
+++ Library/Script/Utility/StreamHolder.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Ghost.Utility
+{
+	public class StreamHolder : ResourceHolder
+	{
+		private Stream stream;
+
+		public StreamHolder(Stream s)
+		{
+			#if DEBUG
+			Debug.Assert(null != s);
+			#endif // DEBUG
+			stream = s;
+		}
+
+		private void ReleaseStream()
+		{
+			stream.Close();
+			stream.Dispose();
+		}
+
+		#region override
+		protected override void ReleaseManagedResource ()
+		{
+			base.ReleaseManagedResource();
+			ReleaseStream();
+		}
+		#endregion override
+	}
+} // namespace Ghost.Utility
