679f3016e26f346b3e51ade934e75335d3d69c55
Evil.Ghost <shawn.ghost.lee@gmail.com>
2016-04-26 18:29:30 +0800

    使用授时服务测试TCPSession

--- Sample/Network/SampleTCPSession.cs
+++ Sample/Network/SampleTCPSession.cs
@@ -1,11 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 using System.Net.Sockets;
+using System.Text;
+using System.IO;
 
 namespace Ghost.Sample
 {
 	public class SampleTCPSession : TCPSession 
 	{
+		private byte[] buffer = new byte[1024];
+		private MemoryStream stream = new MemoryStream();
 
 		#region override
 		protected override void DoBkgSend (Socket tcp, object[] args)
@@ -15,12 +19,27 @@ namespace Ghost.Sample
 
 		protected override object DoBkgReceive (Socket tcp)
 		{
+			var len = tcp.Receive(buffer);
+			while (buffer.Length == len)
+			{
+				stream.Write(buffer, 0, len);
+			}
+			if (0 < len)
+			{
+				stream.Write(buffer, 0, len);
+			}
+			if (0 < stream.Length)
+			{
+				var str = Encoding.UTF8.GetString(stream.GetBuffer(), 0, System.Convert.ToInt32(stream.Length));
+				stream.SetLength(0);
+				return str;
+			}
 			return null;
 		}
 
 		protected override void HandleReceive (object p)
 		{
-			
+			Debug.LogFormat("<color=green>Receive: </color>\n{0}", System.Convert.ToString(p));
 		}
 		#endregion override
 
