a3b8a962ae1fb2ebe73e9352505ea0e5ad5cf499
Evil.Ghost <shawn.ghost.lee@gmail.com>
2016-06-12 11:39:32 +0800

    测试AssetBundle
    使用pngquant处理png图片对于打包AssetBundle没有任何帮助

---
+++ Test/Editor/TestCommands.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+namespace Ghost.EditorTool
+{
+	public static class E_TestCommands 
+	{
+		[MenuItem("Test/AssetBundle")]
+		static void TestAssetBundle()
+		{
+			BuildPipeline.BuildAssetBundles (
+				Application.streamingAssetsPath, 
+				BuildAssetBundleOptions.UncompressedAssetBundle, 
+				EditorUserBuildSettings.activeBuildTarget);
+		}
+	}
+} // namespace Ghost.EditorTool
